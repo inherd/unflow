de20c33d3ffa76c845e0cd1b3e0e88076b8e14f8
Phodal Huang <h@phodal.com>
2021-05-09 22:27:27 +0800

    feat: add basic config def

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,11 @@ use pest::Parser;
 pub struct IdentParser;
 
 fn main() {
-    let pairs = IdentParser::parse(Rule::ident_list, "a1 b2").unwrap_or_else(|e| panic!("{}", e));
+    let pairs = IdentParser::parse(Rule::start, "id: 123").unwrap_or_else(|e| panic!("{}", e));
 
-    // Because ident_list is silent, the iterator will contain idents
     for pair in pairs {
-        // A pair is a combination of the rule which matched and a span of input
-        println!("Rule:    {:?}", pair.as_rule());
-        println!("Span:    {:?}", pair.as_span());
-        println!("Text:    {}", pair.as_str());
-
-        // A pair can be converted to an iterator of the tokens which make it up:
         for inner_pair in pair.into_inner() {
-            match inner_pair.as_rule() {
-                Rule::alpha => println!("Letter:  {}", inner_pair.as_str()),
-                Rule::digit => println!("Digit:   {}", inner_pair.as_str()),
-                _ => unreachable!()
-            };
+            println!("{:?}", inner_pair.as_rule());
         }
     }
 }
