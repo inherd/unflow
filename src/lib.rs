dde3dac9a833e40187e33441181c845d7e380d75
Phodal Huang <h@phodal.com>
2021-05-10 15:59:17 +0800

    feat: add support for parse name

--- src/lib.rs
+++ src/lib.rs
@@ -21,4 +21,19 @@ width: 1080px
         let result = unflow_parser::parse(data);
         assert_eq!(4, result.config.len())
     }
+
+    #[test]
+    fn should_parse_flow() {
+        let data = r#"flow 登出 {
+    SEE 个人信息面
+    DO [Click] "登出".按钮
+    REACT Success: SHOW "登出成功".Toast with ANIMATE(bounce)
+}
+
+"#;
+
+        let result = unflow_parser::parse(data);
+        assert_eq!(1, result.flows.len());
+        assert_eq!("登出", result.flows[0].name)
+    }
 }
--- src/unflow_parser.rs
+++ src/unflow_parser.rs
@@ -6,18 +6,36 @@ use antlr_rust::token_factory::ArenaCommonFactory;
 use antlr_rust::tree::{ParseTree, ParseTreeVisitor, Visitable};
 use serde::{Deserialize, Serialize};
 
-use crate::{Config_declContext, DesignLexer, DesignParser, DesignParserContextType, DesignVisitor};
-use crate::Config_declContextAttrs;
+use crate::{Config_declContext, DesignLexer, DesignParser, DesignParserContextType, DesignVisitor, Flow_DeclContext};
+use crate::{
+    Config_declContextAttrs,
+    Flow_DeclContextAttrs
+};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct UiFlow {
+    pub name: String,
+}
+
+impl UiFlow {
+    fn new(name: String) -> Self {
+        UiFlow {
+            name
+        }
+    }
+}
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Unflow {
-    pub config: HashMap<String, String>
+    pub config: HashMap<String, String>,
+    pub flows: Vec<UiFlow>
 }
 
 impl Default for Unflow {
     fn default() -> Self {
         Unflow {
-            config: Default::default()
+            config: Default::default(),
+            flows: vec![]
         }
     }
 }
@@ -53,5 +71,10 @@ impl<'i> DesignVisitor<'i> for UnflowParser<'i> {
             ctx.config_key().unwrap().get_text(),
             ctx.config_value().unwrap().get_text(),
         );
+    }
+
+    fn visit_flow_Decl(&mut self, ctx: &Flow_DeclContext<'i>) {
+        let flow_name = ctx.IDENTIFIER().unwrap().get_text();
+        self.flow.flows.push(UiFlow::new(flow_name));
     }
 }
