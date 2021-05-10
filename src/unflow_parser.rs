84ff8865dceff0e922290c1eadaa681abdebaa23
Phodal Huang <h@phodal.com>
2021-05-10 14:49:16 +0800

    feat: add basic demos for testings

--- src/unflow_parser.rs
+++ src/unflow_parser.rs
@@ -4,7 +4,8 @@ use antlr_rust::common_token_stream::CommonTokenStream;
 use antlr_rust::InputStream;
 use antlr_rust::tree::{ParseTreeListener, ParseTree};
 
-use crate::{DesignLexer, DesignParserContextType, DesignParser, DesignListener, Config_declContext};
+use crate::{DesignLexer, DesignParserContextType, DesignParser, DesignListener, Config_declContext, DeclarationsContext, designparser};
+use antlr_rust::parser::ParserNodeType;
 
 pub struct UnflowParser {
     pub config: HashMap<String, String>,
@@ -23,11 +24,23 @@ impl UnflowParser {
 
 
 impl<'input> ParseTreeListener<'input, DesignParserContextType> for UnflowParser {
-
+    fn enter_every_rule(&mut self, ctx: &<DesignParserContextType as ParserNodeType<'input>>::Type) {
+        println!(
+            "rule entered {}",
+            designparser::ruleNames
+                .get(ctx.get_rule_index())
+                .unwrap_or(&"error")
+        );
+        println!("value: {:?}", ctx.get_text());
+    }
 }
 
 impl<'input> DesignListener<'input> for UnflowParser {
-    fn enter_config_decl(&mut self, ctx: &Config_declContext<'input>) {
-        println!("{:?}", ctx.get_text());
+    fn enter_config_decl(&mut self, _ctx: &Config_declContext<'input>) {
+        // println!("{:?}", ctx.get_text());
+    }
+
+    fn enter_declarations(&mut self, _ctx: &DeclarationsContext<'input>) {
+        // println!("{:?}", ctx.get_text());
     }
 }
