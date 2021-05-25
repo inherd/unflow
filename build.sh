489718507c5ccda6f87be45b7d4ff362e04d3586
Phodal Huang <h@phodal.com>
2021-05-25 12:42:22 +0800

    docs: fix add antlr rust log

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
+# download from: https://github.com/rrevenantt/antlr4rust
 java -jar ~/Downloads/antlr4-4.8-2-SNAPSHOT-complete.jar -Dlanguage=Rust crates/unflow-parser/src/grammar/Design.g4 -visitor
